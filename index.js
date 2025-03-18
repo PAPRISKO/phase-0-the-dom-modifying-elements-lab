3a474071732ea1ab2c38f61351b7132cf43b9b84
paprisko <jackmchussein965@gmail.com>
2025-03-18 03:32:43 +0300

    assignment

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ if (main) {
 // Create a new <h1> element
 const newHeader = document.createElement("h1");
 
-// Set the id of the new <h1> to "victory"
 newHeader.id = "victory";
 
-// Set the text inside the <h1> to "YOUR-NAME is the champion"
-newHeader.textContent = "YOUR-NAME is the champion"; // Replace YOUR-NAME
+newHeader.textContent = "YOUR-NAME is the champion";
 
-// Append the newHeader to the body
 document.body.appendChild(newHeader);
